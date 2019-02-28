c884ed54bf118ca2ddd518ee1ee4bb7d79b9ed0b
DESKTOP-AHQ5MNJ\bigpo <bigpong88508@gmail.com>
2019-03-01 00:38:28 +0800

    Monster Test 3/1

--- Assets/Scripts/Monster/MonsterBasic.cs
+++ Assets/Scripts/Monster/MonsterBasic.cs
@@ -1,17 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MonsterBasic : MonoBehaviour {
 
     public GameObject player;
+    public Vector3[] patrolArray;
+    NavMeshAgent navMesh;
+    int patrolPoint = 0;
+
+    enum Attackstates
+    {
+        Patrol,
+        Alert,
+        Attacking
+    }
+
+    Attackstates attackstates;
 	// Use this for initialization
 	void Start () {
-		
+        player = GameObject.FindGameObjectWithTag("Player");
+        navMesh = GetComponent<NavMeshAgent>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        switch (attackstates)
+        {
+            case Attackstates.Patrol:
+                e_Patrol();
+                break;
+            case Attackstates.Alert:
+                break;
+            case Attackstates.Attacking:
+                break;
+        }
+        e_Patrol();
 	}
+
+    void e_Patrol()
+    {
+        navMesh.SetDestination(patrolArray[patrolPoint]);
+    }
+
+    void e_Alert()
+    {
+
+    }
+
+    void e_Attacking()
+    {
+
+    }
 }
