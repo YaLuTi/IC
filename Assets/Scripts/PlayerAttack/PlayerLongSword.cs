fb795c7b5bd8e1958d0970d48f0852bb02c23fc2
DESKTOP-AHQ5MNJ\bigpo <bigpong88508@gmail.com>
2019-03-05 20:14:59 +0800

    3/5 Attack Complete

--- Assets/Scripts/PlayerAttack/PlayerLongSword.cs
+++ Assets/Scripts/PlayerAttack/PlayerLongSword.cs
@@ -2,15 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class PlayerLongSword : MonoBehaviour {
 
+    Animator animator;
+    int AttackCombo = 0;
+    bool SwitchState = true;
+    AnimatorStateInfo animatorState;
 	// Use this for initialization
 	void Start () {
-		
+        animator = GetComponent<Animator>();
+        animatorState = animator.GetCurrentAnimatorStateInfo(0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetButtonDown("JoyStickTriangle"))
+        {
+            StopAllCoroutines();
+            if (AttackCombo < 2)
+            {
+                AttackCombo++;
+            }
+            animator.SetInteger("Attack", AttackCombo);
+            StartCoroutine(AttackReset());
+        }
+        CheckAnimatorState();
 	}
+
+    void CheckAnimatorState()
+    {
+        if(animatorState.shortNameHash != animator.GetCurrentAnimatorStateInfo(0).shortNameHash)
+        {
+            animatorState = animator.GetCurrentAnimatorStateInfo(0);
+            animator.SetInteger("Attack", 0);
+            // StartCoroutine(AttackCooldownCounter());
+            SwitchState = true;
+        }
+    }
+
+    IEnumerator AttackReset()
+    {
+        yield return new WaitForSecondsRealtime(0.65f);
+        animator.SetInteger("Attack", 0);
+        AttackCombo = 0;
+        yield return 0;
+    }
+
+    IEnumerator AttackCooldownCounter()
+    {
+        yield return new WaitForFixedUpdate();
+        Debug.Log(1);
+        yield return 0;
+    }
 }
