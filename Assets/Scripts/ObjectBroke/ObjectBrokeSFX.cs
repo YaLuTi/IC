b92d10133ffbdc71013c95fa3620dbd12429e32e
armanixi <36953037+armanixi@users.noreply.github.com>
2019-04-26 22:10:01 +0800

    lightmap fuck u unity and lazy cat 4/26

---
+++ Assets/Scripts/ObjectBroke/ObjectBrokeSFX.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectBrokeSFX : MonoBehaviour {
+
+    public AudioAssets assets;
+	// Use this for initialization
+	void Start () {
+        AudioSource audio = GetComponent<AudioSource>();
+        assets.Play(audio);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ObjectBroke/Slicer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slicer : MonoBehaviour {
+
+    public GameObject BreakObject;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Weapon"))
+        {
+            Instantiate(BreakObject, transform.position, transform.rotation);
+            Destroy(this.gameObject);
+        }
+    }
+}
