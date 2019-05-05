3350f552934999f86924e5b63a02e7da28b2a09d
armanixi <36953037+armanixi@users.noreply.github.com>
2019-05-05 08:52:06 +0800

    505

--- Assets/Scripts/Camera/EZCamera.cs
+++ Assets/Scripts/Camera/EZCamera.cs
@@ -8,8 +8,10 @@ public class EZCamera : MonoBehaviour {
     
     // Use this for initialization
     void Start () {
-		
-	}
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
 	
 	// Update is called once per frame
 	void Update ()
