de77d65f936a31b8f7e17c40a833433817aacf56
armanixi <36953037+armanixi@users.noreply.github.com>
2019-05-21 09:19:35 +0800

    521 start

--- Assets/Scripts/UI/TitleUI.cs
+++ Assets/Scripts/UI/TitleUI.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+
 public class TitleUI : MonoBehaviour {
 
     public Image panel;
+    public Image BlackPanel;
     public AnimationCurve animationCurve;
+    public GameObject pressText;
+    public GameObject Version;
+    public GameObject Loading;
     float time;
     AudioSource audioSource;
     bool In = false;
 	// Use this for initialization
 	void Start () {
-        panel.color = new Color(1, 1, 1, 1);
+        BlackPanel.color = new Color(0, 0, 0, 1);
         audioSource = GetComponent<AudioSource>();
 	}
 	
@@ -30,6 +35,7 @@ public class TitleUI : MonoBehaviour {
         {
             if (time < 1)
             {
+                BlackPanel.color = new Color(0, 0, 0, animationCurve.Evaluate(time));
                 time += Time.deltaTime;
                 if (time > 1)
                 {
@@ -42,6 +48,10 @@ public class TitleUI : MonoBehaviour {
             if (time > 0)
             {
                 time -= Time.deltaTime;
+                pressText.SetActive(false);
+                Version.SetActive(false);
+                Loading.SetActive(true);
+                panel.color = new Color(1, 1, 1, animationCurve.Evaluate(time));
                 if (time < 0)
                 {
                     time = 0;
@@ -49,11 +59,24 @@ public class TitleUI : MonoBehaviour {
                 }
             }
         }
-        panel.color = new Color(1, 1, 1, animationCurve.Evaluate(time));
 	}
 
     IEnumerator DisplayLoadingScreen(string sceneName)////(1)
     {
+        yield return new WaitForSecondsRealtime(1.5f);
+
+        while(BlackPanel.color.a < 1)
+        {
+            Color c = BlackPanel.color;
+            c.a += (1 - c.a) * 0.05f;
+            if(c.a > 0.9f)
+            {
+                c.a = 1;
+            }
+            BlackPanel.color = c;
+            yield return 0;
+        }
+
         AsyncOperation async = SceneManager.LoadSceneAsync(sceneName);////(2)
         while (!async.isDone)////(3)
         {
