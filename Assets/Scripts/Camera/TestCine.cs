f3071870c84304b7074de4bd48342cf6f99210ab
armanixi <36953037+armanixi@users.noreply.github.com>
2019-05-22 08:46:03 +0800

    522

---
+++ Assets/Scripts/Camera/TestCine.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCine : MonoBehaviour {
+
+    public Component component;
+	// Use this for initialization
+	void Start () {
+        //StartCoroutine(enumerator());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator enumerator()
+    {
+        yield return new WaitForSeconds(8.5f);
+        transform.position = new Vector3(0, 0, 0);
+        Destroy(component);
+        yield return 0;
+
+    }
+}
