7dc686b92144d578c717f06d6a89f725d0d71860
zian0912 <a106082045@mail.shu.edu.tw>
2019-10-27 17:59:37 +0800

    shadow dick

--- Assets/Scripts/Camera/EZCamera.cs
+++ Assets/Scripts/Camera/EZCamera.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EZCamera : MonoBehaviour {
+    [Header("Speed")]
     public float moveSpeed = 0.1f;
     public float RotateSpeed = 1f;
-    
+    public float UpDownSpeed = 0.5f;
+
+    float MoveSpeedMultiplier = 0.5f;
+
+    Camera camera;
+    [Header("Value")]
+    public float MinFieldOfView;
+    public float ViewMultiplier = 1;
+    float MaxFieldOfView;
+
     // Use this for initialization
     void Start () {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-
+        camera = GetComponent<Camera>();
+        MaxFieldOfView = camera.fieldOfView;
     }
 	
 	// Update is called once per frame
@@ -19,16 +30,34 @@ public class EZCamera : MonoBehaviour {
         Vector3 hh = Input.GetAxis("Horizontal") * transform.right;
         Vector3 vv = Input.GetAxis("Vertical") * transform.forward;
         float yy = 0;
-        if (Input.GetButton("Fire1"))
-        {
-            yy = 1;
-        }
+
         if (Input.GetButton("Fire2"))
         {
-            yy = -1;
+            if (camera.fieldOfView > MinFieldOfView) camera.fieldOfView -= (camera.fieldOfView - MinFieldOfView) * 0.15f * ViewMultiplier;
+        }
+        else
+        {
+            if (camera.fieldOfView < MaxFieldOfView) camera.fieldOfView += (MaxFieldOfView - camera.fieldOfView) * 0.25f * ViewMultiplier;
         }
 
-        transform.position += (hh + vv + new Vector3(0, yy, 0)) * moveSpeed;
+        if (Input.GetButton("Jump"))
+        {
+            yy = UpDownSpeed;
+        }
+        if (Input.GetButton("Down"))
+        {
+            yy = -UpDownSpeed;
+        }
+        if (Input.GetButton("Fire3"))
+        {
+            MoveSpeedMultiplier = 0.5f;
+        }
+        else
+        {
+            MoveSpeedMultiplier = 1;
+        }
+
+        transform.position += (hh + vv + new Vector3(0, yy, 0)) * moveSpeed * MoveSpeedMultiplier;
 
         float h = Input.GetAxis("Mouse X");
         float v = Input.GetAxis("Mouse Y");
