755e9dbfbb27c2d56847d046c1837525545d2fde
DESKTOP-AHQ5MNJ\bigpo <bigpong88508@gmail.com>
2019-12-02 21:41:52 +0800

    12/2 BossScene

---
+++ Assets/Scripts/BossEventTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEventTrigger : MonoBehaviour
+{
+    bool IsIn = false;
+    public CutSceneDirector director;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsIn)
+        {
+            if (Input.GetButtonDown("JoyStickO"))
+            {
+                director._BossScene();
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            IsIn = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            IsIn = false;
+        }
+    }
+}
--- Assets/Scripts/CutSceneDirector.cs
+++ Assets/Scripts/CutSceneDirector.cs
@@ -7,21 +7,51 @@ using UnityEngine.UI;
 public class CutSceneDirector : MonoBehaviour
 {
     GameObject UI;
+    public Camera MainCamera;
     [Header("OpenScene")]
     public GameObject CameraOne;
     public GameObject CameraTwo;
     public GameObject ActingPlayer;
     public Image BlackPanel;
+    [Header("Boss Scene")]
+    public GameObject CameraThree;
+    public GameObject CameraFour;
+    public GameObject BossScenePlayer;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(OpenScene());
+        // StartCoroutine(OpenScene());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void _BossScene()
+    {
+        StartCoroutine(BossScene());
+    }
+
+    IEnumerator BossScene()
+    {
+        BlackPanel.DOColor(new Color(0, 0, 0, 1), 0.5f);
+        yield return new WaitForSeconds(1.75f);
+        MainCamera.enabled = false;
+        CameraThree.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        CameraThree.transform.DOMove(new Vector3(-164.42f, 5.28f, 34f), 8).SetEase(Ease.InCubic);
+        BlackPanel.DOColor(new Color(0, 0, 0, 0), 1.5f);
+        yield return new WaitForSeconds(0.25f);
+        BossScenePlayer.GetComponent<Animator>().SetTrigger("Pray");
+        yield return new WaitForSeconds(5f);
+        CameraFour.transform.DOMove(new Vector3(-163.98f, 5.94f, 32.258f), 12f).SetEase(Ease.InCubic);
+        CameraFour.transform.DORotate(new Vector3(-5.051f, -90f, 0), 12).SetEase(Ease.InCubic);
+        CameraThree.SetActive(false);
+        CameraFour.SetActive(true);
+        yield return new WaitForSeconds(10f);
+        yield return 0;
     }
 
     IEnumerator OpenScene()
