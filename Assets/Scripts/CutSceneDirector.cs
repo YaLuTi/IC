7be0f477845e71a40c18fe8f85da21de3bf831de
DESKTOP-AHQ5MNJ\bigpo <bigpong88508@gmail.com>
2019-12-03 20:53:15 +0800

    12/3 Scene JJ

--- Assets/Scripts/CutSceneDirector.cs
+++ Assets/Scripts/CutSceneDirector.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using UnityEngine.UI;
+using TMPro;
 
 public class CutSceneDirector : MonoBehaviour
 {
     GameObject UI;
     public Camera MainCamera;
+    public GameObject BlackBar;
     public GameObject Player;
     public GameObject Cine;
+    public GameObject Good_UI;
     public static bool IsOnMovie = false;
     [Header("OpenScene")]
     public GameObject CameraOne;
     public GameObject CameraTwo;
     public GameObject ActingPlayer;
+    public AK.Wwise.Event FoundSound;
     public Image BlackPanel;
+    public TextMeshProUGUI OpenText;
     [Header("Boss Scene")]
     public GameObject CameraThree;
     public GameObject CameraFour;
@@ -26,7 +31,7 @@ public class CutSceneDirector : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // StartCoroutine(OpenScene());
+        StartCoroutine(OpenScene());
     }
 
     // Update is called once per frame
@@ -45,6 +50,7 @@ public class CutSceneDirector : MonoBehaviour
         IsOnMovie = true;
         BlackPanel.DOColor(new Color(0, 0, 0, 1), 0.5f);
         yield return new WaitForSeconds(1.75f);
+        BlackBar.SetActive(true);
         BossScenePlayer.SetActive(true);
         MainCamera.gameObject.SetActive(false);
         CameraThree.SetActive(true);
@@ -73,6 +79,7 @@ public class CutSceneDirector : MonoBehaviour
         Cine.transform.eulerAngles = new Vector3(13, 90, 0);
         yield return new WaitForSeconds(3.5f);
         Boss.SetActive(true);
+        BlackBar.SetActive(false);
         CameraFive.SetActive(false);
         MainCamera.gameObject.SetActive(true);
         BlackPanel.DOColor(new Color(0, 0, 0, 0), 2f);
@@ -85,14 +92,37 @@ public class CutSceneDirector : MonoBehaviour
     IEnumerator OpenScene()
     {
         IsOnMovie = true;
+        ActingPlayer.SetActive(true);
+        Good_UI.SetActive(false);
+        BlackPanel.DOColor(new Color(0, 0, 0, 1), 0);
+        BlackBar.SetActive(true);
+        BlackPanel.DOColor(new Color(0, 0, 0, 0), 0.5f);
+        CameraOne.SetActive(true);
+        MainCamera.gameObject.SetActive(false);
         CameraOne.transform.DOMove(new Vector3(32.11f, 0.855f, 15.917f), 10f).SetEase(Ease.InOutSine);
         yield return new WaitForSecondsRealtime(9.5f);
         CameraOne.SetActive(false);
         CameraTwo.SetActive(true);
-        CameraTwo.transform.DOMove(new Vector3(38.882f, 2.793f, 16.012f), 7f);
+        CameraTwo.transform.DOMove(new Vector3(38.677f, 0.65f, 16.012f), 14.954f);
         ActingPlayer.GetComponent<Animator>().SetTrigger("StandUp");
         yield return new WaitForSecondsRealtime(6f);
         BlackPanel.DOColor(new Color(0, 0, 0, 1), 2f);
+        yield return new WaitForSecondsRealtime(2f);
+        BlackBar.SetActive(false);
+        yield return new WaitForSecondsRealtime(1f);
+        CameraOne.SetActive(false);
+        MainCamera.gameObject.SetActive(true);
+        ActingPlayer.SetActive(false);
+        BlackPanel.DOColor(new Color(0, 0, 0, 0), 2f);
+        IsOnMovie = false;
+        Good_UI.SetActive(true);
+        yield return new WaitForSecondsRealtime(0.7f);
+        FoundSound.Post(gameObject);
+        yield return new WaitForSecondsRealtime(0.5f);
+        OpenText.DOColor(new Color(1, 1, 1, 1), 1f).SetEase(Ease.OutQuint);
+        yield return new WaitForSecondsRealtime(0.05f);
+        yield return new WaitForSecondsRealtime(3.5f);
+        OpenText.DOColor(new Color(0, 0, 0, 0), 1f);
         yield return 0;
     }
 }
